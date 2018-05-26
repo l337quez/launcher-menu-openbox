9edaf13a4dc4a46ef6deda10949a6949f10e2fd4
l337quez <l337.ronald@gmail.com>
2018-05-26 13:08:48 -0400

    update v2.0

---
+++ Menu-openbox.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+clear
+echo “Instalacion del Menu en tu Escritorio Openbox”
+echo "Script escrito en python3"
+
+#copiamos el launcher
+cp Menu.desktop /home/$USER/.local/share/applications/
+
+echo "hacemos una copia del launcher al escritorio"
+cp Menu.desktop /home/$USER/.local/share/applications/
+
+#copiamos la imagen del launcher
+cp arch.png /home/$USER/.local/share/icons/
+
+
+mkdir /home/$USER/.local/Menu
+cp menu_plank_openbox.py /home/$USER/.local/Menu
+
+
+#verificamos si tiene instalado plank
+
+if `cd /home/$USER/.local/share/plank >/dev/null`
+then
+	echo "agregando launcher al dock plank"
+	cd plank
+	cp Menu.dockitem /home/$USER/.config/plank/dock1/launchers/
+else
+	echo "No tienes instalado plank"
+	echo "el launcher lo encontraras en el escritorio"
+fi
+
+
+
+echo "https://ronaldl337.wordpress.com/"
+echo "Hecho por Ing.Ronal Forero"
+echo “Instalacion Terminada”
